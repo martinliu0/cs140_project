6505d869ce0487e062e8ffc0f403f40cb975fab2
gogate17 <lwasserman@colgate.edu>
2016-05-09 14:51:22 +0100

    more CSS and functionality

--- crispyduck/recipes/views.py
+++ crispyduck/recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.urlresolvers import reverse
 from urllib.parse import urlencode
-from django.contrib.messages import info, warning, success
+from django.contrib.messages import info, warning, success, error
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import MainForm, ReviewForm
